bad99c5bb34ce48a454ae166277069e9b80b71a8
g.robinson@f5.com <Seattle2016!>
2020-07-20 15:30:36 -0400

    formatting cleanup

--- as3_inventory.py
+++ as3_inventory.py
@@ -19,7 +19,7 @@ auth_token = json.loads(token_auth_response.text)['token']['token']
 headers = {'content-type': 'application/json', 'X-F5-Auth-Token': auth_token}
 global_config_sets_response = requests.get('https://' + bigiq_ip + '/mgmt/cm/global/config-sets', verify=False,
                                            headers=headers)
-print(global_config_sets_response.content)
+# print(global_config_sets_response.content)
 global_config_sets = json.loads(global_config_sets_response.content)['items']
 for current_config_set in global_config_sets:
     # Template for attributes
@@ -37,7 +37,7 @@ for current_config_set in global_config_sets:
         print('    ID: ' + current_config_set_id)
     if current_config_set.has_key('status'):
         current_config_set_status = current_config_set['status']
-        print('    Status: ' + current_config_set_status)    
+        print('    Status: ' + current_config_set_status)
     if current_config_set.has_key('applicationServiceType'):
         current_config_set_last_deployment_time = current_config_set['applicationServiceType']
         print('    Application Service Type: ' + current_config_set_last_deployment_time)
@@ -49,34 +49,28 @@ for current_config_set in global_config_sets:
         print('    Protection Mode: ' + current_config_set_last_deployment_time)
     if current_config_set.has_key('applicationReference'):
         current_config_set_application_reference = current_config_set['applicationReference']['link']
-        current_config_set_application = requests.get(
-            current_config_set_application_reference.replace('localhost', bigiq_ip), verify=False, headers=headers)
+        current_config_set_application = requests.get(current_config_set_application_reference.replace('localhost', bigiq_ip), verify=False, headers=headers)
         current_config_set_application_name = json.loads(current_config_set_application.content)['name']
         print('    Application: ' + current_config_set_application_name)
     if current_config_set.has_key('tenantReference'):
         current_config_set_tenant_reference = current_config_set['tenantReference']['link']
-        current_config_set_tenant = requests.get(current_config_set_tenant_reference.replace('localhost', bigiq_ip),
-                                                 verify=False, headers=headers)
+        current_config_set_tenant = requests.get(current_config_set_tenant_reference.replace('localhost', bigiq_ip),verify=False, headers=headers)
         current_config_set_tenant_name = json.loads(current_config_set_tenant.content)['name']
         print('    Tenant: ' + current_config_set_tenant_name)
     if current_config_set.has_key('appSvcsTemplateReference'):
         current_config_set_app_svcs_template_reference = current_config_set['appSvcsTemplateReference']['link']
-        current_config_set_app_svcs_template = requests.get(
-            current_config_set_app_svcs_template_reference.replace('localhost', bigiq_ip), verify=False,
-            headers=headers)
+        current_config_set_app_svcs_template = requests.get(current_config_set_app_svcs_template_reference.replace('localhost', bigiq_ip), verify=False,headers=headers)
         current_config_set_app_svcs_template_name = json.loads(current_config_set_app_svcs_template.content)['name']
         print('    Application Services (AS3) Template: ' + current_config_set_app_svcs_template_name)
     if current_config_set.has_key('templateReference'):
         current_config_set_template_reference = current_config_set['templateReference']['link']
         current_config_set_template = requests.get(
-            current_config_set_template_reference.replace('localhost', bigiq_ip), verify=False,
-            headers=headers)
+            current_config_set_template_reference.replace('localhost', bigiq_ip), verify=False,headers=headers)
         current_config_set_template_name = json.loads(current_config_set_template.content)['name']
         print('    BIG-IQ Application Template: ' + current_config_set_app_svcs_template_name)
     if current_config_set.has_key('deviceReference'):
         current_config_set_device_reference = current_config_set['deviceReference']['link']
-        current_config_set_device = requests.get(current_config_set_device_reference.replace('localhost', bigiq_ip),
-                                                 verify=False, headers=headers)
+        current_config_set_device = requests.get(current_config_set_device_reference.replace('localhost', bigiq_ip),verify=False, headers=headers)
         current_config_set_device_name = json.loads(current_config_set_device.content)['address']
         print('    Device: ' + current_config_set_device_name)
     if current_config_set.has_key('selfLink'):
