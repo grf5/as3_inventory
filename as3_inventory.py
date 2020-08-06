8079f9a699d9e4dee3b38edc8193bb3fb8e6b83f
g.robinson@f5.com <Seattle2016!>
2020-08-06 12:49:40 -0400

    adding resource processing

--- as3_inventory.py
+++ as3_inventory.py
@@ -11,12 +11,14 @@ urllib3.disable_warnings()
 # Get an auth token from BIG-IQ
 credentials_dict = {"username": username, "password": password}
 headers = {'content-type': 'application/json'}
-token_auth_response = requests.post('https://' + bigiq_ip + '/mgmt/shared/authn/login', verify=False,data=json.dumps(credentials_dict), headers=headers)
+token_auth_response = requests.post('https://' + bigiq_ip + '/mgmt/shared/authn/login', verify=False,
+                                    data=json.dumps(credentials_dict), headers=headers)
 auth_token = json.loads(token_auth_response.text)['token']['token']
 
 # Get a list of all of the applications deployed on the BIG-IQ
 headers = {'content-type': 'application/json', 'X-F5-Auth-Token': auth_token}
-global_config_sets_response = requests.get('https://' + bigiq_ip + '/mgmt/cm/global/config-sets', verify=False,headers=headers)
+global_config_sets_response = requests.get('https://' + bigiq_ip + '/mgmt/cm/global/config-sets', verify=False,
+                                           headers=headers)
 global_config_sets = json.loads(global_config_sets_response.content)['items']
 for current_config_set in global_config_sets:
     # Template for attributes
@@ -46,27 +48,33 @@ for current_config_set in global_config_sets:
         print('    Protection Mode: ' + current_config_set_protection_mode)
     if current_config_set.has_key('applicationReference'):
         current_config_set_application_reference = current_config_set['applicationReference']['link']
-        current_config_set_application = requests.get(current_config_set_application_reference.replace('localhost', bigiq_ip), verify=False, headers=headers)
+        current_config_set_application = requests.get(
+            current_config_set_application_reference.replace('localhost', bigiq_ip), verify=False, headers=headers)
         current_config_set_application_name = json.loads(current_config_set_application.content)['name']
         print('    Application: ' + current_config_set_application_name)
     if current_config_set.has_key('tenantReference'):
         current_config_set_tenant_reference = current_config_set['tenantReference']['link']
-        current_config_set_tenant = requests.get(current_config_set_tenant_reference.replace('localhost', bigiq_ip),verify=False, headers=headers)
+        current_config_set_tenant = requests.get(current_config_set_tenant_reference.replace('localhost', bigiq_ip),
+                                                 verify=False, headers=headers)
         current_config_set_tenant_name = json.loads(current_config_set_tenant.content)['name']
         print('    Tenant: ' + current_config_set_tenant_name)
     if current_config_set.has_key('appSvcsTemplateReference'):
         current_config_set_app_svcs_template_reference = current_config_set['appSvcsTemplateReference']['link']
-        current_config_set_app_svcs_template = requests.get(current_config_set_app_svcs_template_reference.replace('localhost', bigiq_ip), verify=False,headers=headers)
+        current_config_set_app_svcs_template = requests.get(
+            current_config_set_app_svcs_template_reference.replace('localhost', bigiq_ip), verify=False,
+            headers=headers)
         current_config_set_app_svcs_template_name = json.loads(current_config_set_app_svcs_template.content)['name']
         print('    Application Services (AS3) Template: ' + current_config_set_app_svcs_template_name)
     if current_config_set.has_key('templateReference'):
         current_config_set_template_reference = current_config_set['templateReference']['link']
-        current_config_set_template = requests.get(current_config_set_template_reference.replace('localhost', bigiq_ip), verify=False,headers=headers)
+        current_config_set_template = requests.get(current_config_set_template_reference.replace('localhost', bigiq_ip),
+                                                   verify=False, headers=headers)
         current_config_set_template_name = json.loads(current_config_set_template.content)['name']
         print('    BIG-IQ Application Template: ' + current_config_set_template_name)
     if current_config_set.has_key('deviceReference'):
         current_config_set_device_reference = current_config_set['deviceReference']['link']
-        current_config_set_device = requests.get(current_config_set_device_reference.replace('localhost', bigiq_ip),verify=False, headers=headers)
+        current_config_set_device = requests.get(current_config_set_device_reference.replace('localhost', bigiq_ip),
+                                                 verify=False, headers=headers)
         current_config_set_device_name = json.loads(current_config_set_device.content)['hostname']
         current_config_set_device_address = json.loads(current_config_set_device.content)['address']
         print('    Device: ' + current_config_set_device_name + '(' + current_config_set_device_address + ')')
@@ -91,17 +99,23 @@ for current_config_set in global_config_sets:
         print(current_config_set_resource_dict)
         for current_object in current_config_set_resource_dict:
             if current_object[:11] == 'ltm:virtual':
-                print('    Virtual Server: ' + str(current_config_set_resource_dict[current_object][0]['parameters']['name'] + ' (' + current_config_set_resource_dict[current_object][0]['parameters']['destinationAddress'] + ':' + str(current_config_set_resource_dict[current_object][0]['parameters']['destinationPort'] + '/' + current_config_set_resource_dict[current_object][0]['parameters']['mask'] + ')')))
+                print('    Virtual Server: ' + str(
+                    current_config_set_resource_dict[current_object][0]['parameters']['name'] + ' (' +
+                    current_config_set_resource_dict[current_object][0]['parameters']['destinationAddress'] + ':' + str(
+                        current_config_set_resource_dict[current_object][0]['parameters']['destinationPort'] + '/' +
+                        current_config_set_resource_dict[current_object][0]['parameters']['mask'] + ')')))
             if current_object[:8] == 'ltm:pool':
                 print('    Pool: ' + str(current_config_set_resource_dict[current_object][0]['parameters']['name']))
-                pool_subcollection_resources = current_config_set_resource_dict[current_object][0]['subcollectionResources']
+                pool_subcollection_resources = current_config_set_resource_dict[current_object][0][
+                    'subcollectionResources']
                 for current_pool_subcollection_resource in pool_subcollection_resources:
                     if current_pool_subcollection_resource[:8] == 'members:':
                         pool_member_list = pool_subcollection_resources[current_pool_subcollection_resource]
-                        for current_pool_member in pool_member_list:                            
+                        for current_pool_member in pool_member_list:
                             print('    - ' + str(current_pool_member['parameters']['nodeReference']['link']))
             if current_object[:8] == 'ltm:node':
                 print('    Nodes:')
                 node_list = current_config_set_resource_dict[current_object]
                 for current_node in node_list:
-                    print('    - ' + str(current_node['parameters']['name']) + '(' + str(current_node['parameters']['address']) + ')')
+                    print('    - ' + str(current_node['parameters']['name']) + '(' + str(
+                        current_node['parameters']['address']) + ')')
