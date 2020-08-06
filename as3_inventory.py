75f1b23b720fff5daa2c57ba9ead39d465184b58
g.robinson@f5.com <Seattle2016!>
2020-08-06 12:11:05 -0400

    adding resource processing

--- as3_inventory.py
+++ as3_inventory.py
@@ -91,7 +91,7 @@ for current_config_set in global_config_sets:
         print(current_config_set_resource_dict)
         for current_object in current_config_set_resource_dict:
             if current_object[:11] == 'ltm:virtual':
-                print('    Virtual Server: ' + str(current_config_set_resource_dict[current_object][0]['parameters']['name'] + ' (' + current_config_set_resource_dict[current_object][0]['parameters']['destinationAddress'] + ')'))
+                print('    Virtual Server: ' + str(current_config_set_resource_dict[current_object][0]['parameters']['name'] + ' (' + current_config_set_resource_dict[current_object][0]['parameters']['destinationAddress'] + '/' + current_config_set_resource_dict[current_object][0]['parameters']['mask'] + ')'))
             if current_object[:8] == 'ltm:pool':
                 print('    Pool: ' + str(current_config_set_resource_dict[current_object][0]))
             if current_object[:8] == 'ltm:node':
